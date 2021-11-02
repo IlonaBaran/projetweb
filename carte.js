681d740a36728e7b5a2af7b46c9be5fddd4af8fe
Bmaeve <maeve.blarel@ensg.eu>
2021-11-02 23:02:46 +0100

    Modification-avancee_2021-11-02

--- carte.js
+++ carte.js
@@ -10,15 +10,51 @@ L.tileLayer('https://wxs.ign.fr/essentiels/geoportail/wmts?layer=GEOGRAPHICALGRI
 }).addTo(map);
 
 // Pour mettre une image dans la carte 
-var greenIcon = L.icon({
-    iconUrl: 'coeur.jpg',
+var coeurIcon = L.icon({
+    iconUrl: 'images/coeur.jpg',
     iconSize:     [38, 95], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    
 });
-L.marker([14.671891, -61.093182], {icon: greenIcon}).addTo(map);
+L.marker([14.671891, -61.093182], {icon: coeurIcon}).addTo(map);
+
+
+let data = new FormData();
+fetch('objet.php', {
+    method: 'post',
+    body: data
+})
+.then(response => response.json())
+.then( result => {
+    console.log('rrr');
+    result.forEach((elem) => {
+        // Image de Mirabelle
+        console.log(elem['longitude']);
+        var mirabelleIcon = L.icon({
+            iconUrl: elem['icone'],
+            iconSize: [38, 95], // size of the icon
+            iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+            popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+        
+        });
+        console.log('rrr34');
+        L.marker([elem['latitude'], elem['longitude']], {icon: mirabelleIcon}).addTo(map);
+    });
+    console.log('rrr4');
+});
+console.log('rrr56');
+
+var mirabelle2Icon = L.icon({
+    iconUrl: "images/mirabelle.jpg",
+    iconSize: [38, 95], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+});
+L.marker([40, 10], {icon: mirabelle2Icon}).addTo(map);
 
 $map.addEventListener("clik", () => {
-    $coeur.style.display = 'none';
+    $coeur.style.visibility = 'hidden';
     }
 )
--- carte.php
+++ carte.php
@@ -2,8 +2,7 @@
   <html lang="fr">
     <head>
       <meta charset="UTF-8">
-      <title>leaflet.html</title>
-
+      <title>EscapeGameOnEstLa</title>
       <link rel="stylesheet" href="styleCarte.css">
       <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
       integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
@@ -15,34 +14,19 @@
     </head>
 
     <body>
-      <div id="titre"><?php echo "titre:  Vous jouez en tant que $_GET[login]" ?>
-      </div>
-
-      <div id="map">
-      </div>
-
+      <div id="titre"><?php echo "titre:  Vous jouez en tant que "//$_GET[login]" ?></div>
+      <div id="map"></div>
       <?php     
-      echo "
-      <div id=\"pourPasserALaPageSuivantePourLeMoment\">
-      <form id=\"identifiantForm\" method=\"get\" action=\"pageFin.php\">
-              <input type=\"readonly\" name=\"login\" value=\"$_GET[login]\" style=\"display:none;\">
-              <input type=\"submit\" value=\"OK\">
-      </form>
-      </div>";
+      //echo " <div id=\"pourPasserALaPageSuivantePourLeMoment\"> <form id=\"identifiantForm\" method=\"get\" action=\"pageFin.php\"> <input type=\"readonly\" name=\"login\" value=\"$_GET[login]\" style=\"display:none;\"> <input type=\"submit\" value=\"OK\"> </form> </div>";
       ?>
-
-      <div id="chrono"><p>chrono</p>
-      </div>
-
-      <div id="niveaux"><p>niveaux</p>
-      </div>
-
-      <div id="bus"><p>bus</p>
-      </div>
-
-      <div id="indice"><p>indice</p>
-      </div>
-
+      <div id="chrono"><p>Chrono</p></div>
+      <div id="niveaux"><p>Niveaux</p></div>
+      <div id="bus"><p>Bus</p></div>
+      <div id="indice"><p>Indice</p></div>
+      <div id="inventaire"><p>Inventaire</p></div>
+      <?php
+        include("objet.php");
+      ?>
       <script src="carte.js"></script>
   	</body>
 </html>
